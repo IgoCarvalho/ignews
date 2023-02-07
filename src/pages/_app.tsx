c4535b62ec2d34c2c27171cc37faba07fc6ff68d
Igo Carvalho <igocarvalho00@gmail.com>
2023-02-07 18:08:38 -0300

    feat: add sass and global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,27 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
+import { Roboto } from '@next/font/google';
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700', '900'] });
+
+import '@/styles/global.scss';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <style>
+          {`
+            html {
+              font-family: ${roboto.style.fontFamily};
+            }
+          `}
+        </style>
+
+        <title>ignews</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
