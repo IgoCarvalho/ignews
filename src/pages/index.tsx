9373347189e2b1b076690213a0ed2b4874dfdb6f
Igo Carvalho <igocarvalho00@gmail.com>
2023-02-07 21:42:45 -0300

    feat: create Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import avatarImg from '@/assets/images/avatar.svg';
+
+import styles from '@/styles/Home.module.scss';
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | ignews</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+
+          <h1>
+            News about <br /> the <span>React</span> world
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src={avatarImg} alt="Girl coding" />
+      </main>
+    </>
+  );
 }
