df5d60419b50f95b2118c887336d8af8bd0c2b05
Igo Carvalho <igocarvalho00@gmail.com>
2023-02-09 17:17:44 -0300

    feat: add stripe

---
+++ src/services/stripe.ts
@@ -0,0 +1,11 @@
+import Stripe from 'stripe';
+
+import packageJson from '../../package.json';
+
+export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
+  apiVersion: '2022-11-15',
+  appInfo: {
+    name: packageJson.name,
+    version: packageJson.version,
+  },
+});
