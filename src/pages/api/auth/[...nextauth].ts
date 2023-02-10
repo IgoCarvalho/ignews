ad0c19bf66638ecb691e68a569f0580807b25f61
Igo Carvalho <igocarvalho00@gmail.com>
2023-02-10 11:36:19 -0300

    feat: save user email in faunadb on signIn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,40 @@
-import NextAuth from 'next-auth';
+import { query as q } from 'faunadb';
+import NextAuth, { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-export const authOptions = {
+import { fauna } from '@/services/fauna';
+
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    signIn({ user }) {
+      if (!user.email) {
+        return false;
+      }
+
+      const userByEmailQuery = q.Match(q.Index('user_by_email'), q.Casefold(user.email));
+      const usersCollection = q.Collection('Users');
+
+      try {
+        fauna.query(
+          q.If(
+            q.Not(q.Exists(userByEmailQuery)),
+            q.Create(usersCollection, { data: { email: user.email } }),
+            q.Get(userByEmailQuery)
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
