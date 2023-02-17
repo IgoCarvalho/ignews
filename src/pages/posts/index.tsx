87507fe799d170d746aef8e382a69e446d9cb3c6
Igo Carvalho <igocarvalho00@gmail.com>
2023-02-17 16:50:20 -0300

    feat: list posts from prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import * as prismicH from '@prismicio/helpers';
+
+import { getPrismicClient } from '@/services/prismic';
+
 import styles from './Posts.module.scss';
 
-export default function Posts() {
+interface Post {
+  slug: string;
+  title: string;
+  except: string;
+  updatedAt: string;
+}
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,32 +25,47 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage multiple packages
-              with a shared build, test, and release process.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage multiple packages
-              with a shared build, test, and release process.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage multiple packages
-              with a shared build, test, and release process.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.except}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getAllByType('post', { fetch: ['post.title', 'post.content'] });
+
+  const posts = response.map((post) => {
+    const exceptText =
+      post.data.content.find((content: Record<string, string>) =>
+        /paragraph|heading/i.test(content.type)
+      )?.text ?? '';
+
+    const postDate = new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    });
+
+    return {
+      slug: post.uid,
+      title: prismicH.asText(post.data.title),
+      except: exceptText,
+      updatedAt: postDate,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
